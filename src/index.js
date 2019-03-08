2f12627f914fbbfe7990bec682a476ab25e05db6
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-08 08:51:40 +0000

    imported movies component and displayed it
    vid 37

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 //import component ex
-//import Counter from './components/counter';
+import Movies from "./components/movies";
 
 // import bootstrap v4
 import "bootstrap/dist/css/bootstrap.css";
 //font awesome
 import "font-awesome/css/font-awesome.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Movies />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
