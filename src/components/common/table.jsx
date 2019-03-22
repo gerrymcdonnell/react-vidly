59ba022a2f5e8b80fc8e945722cee68a18c58ea5
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-22 17:45:15 +0000

    object destructuring with arguments

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -2,21 +2,18 @@ import React, { Component } from 'react';
 import TableHeader from './tableHeader';
 import TableBody from './tableBody';
 
-const Table = (props) => {
-
-    const {columns,sortColumn,onSort,data}=props;
-
+const Table = ({ columns, sortColumn, onSort, data } ) => {
     return (
         <table className="table">
-                <TableHeader
-                    columns={columns}
-                    sortColumn={sortColumn}
-                    onSort={onSort}
-                />
-                {/**vid 84: create table body component and pass in the movies array */}
-                <TableBody columns={columns} data={data} />
+            <TableHeader
+                columns={columns}
+                sortColumn={sortColumn}
+                onSort={onSort}
+            />
+            {/**vid 84: create table body component and pass in the movies array */}
+            <TableBody columns={columns} data={data} />
         </table>
-      );
+    );
 }
- 
+
 export default Table;
