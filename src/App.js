fecf673f5508a8c5dfd96d5825ba55a1aac52cb4
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-23 12:04:50 +0000

    added bs 4 basic navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
 import "./App.css";
+import Navbar from './components/common/navBar';
 
 class App extends Component {
   render() {
+    
     return (
       /* A JSX comment */
       <main className="container">
+        <Navbar/>
         <Movies />
       </main>
     );
