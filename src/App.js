f2aa529294894ebcce099a70ffb22c5a1577489c
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-23 12:27:44 +0000

    added basic nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+//import route component
+import { Route, Switch, Redirect } from 'react-router-dom'
+
+import NotFound from "./components/notFound";
 import Movies from "./components/movies";
 import "./App.css";
 import Navbar from './components/common/navBar';
 
 class App extends Component {
   render() {
-    
+
     return (
       /* A JSX comment */
       <main className="container">
-        <Navbar/>
+        <Navbar />
         <Movies />
+
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/" exact component={Movies} />
+          <Redirect to="/not-found" />
+        </Switch>
+
+
+
       </main>
     );
   }
--- src/index.js
+++ src/index.js
@@ -9,8 +9,14 @@ import "bootstrap/dist/css/bootstrap.css";
 //font awesome
 import "font-awesome/css/font-awesome.css";
 
-//render the component in the root element of the page
-ReactDOM.render(<App />, document.getElementById("root"));
+//vid 94 type first letter of import i.e rrd then tab
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
