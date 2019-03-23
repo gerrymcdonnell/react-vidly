a01d2967ea9d0831c4285109f4c0280d54c851fb
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-23 13:31:26 +0000

    routing fix with exact keyword

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
+
 //import route component
 import { Route, Switch, Redirect } from 'react-router-dom'
 
 import NotFound from "./components/notFound";
 import Movies from "./components/movies";
-
-import "./App.css";
 import Navbar from './components/navBar';
 import Home from './components/home';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
+
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -18,16 +19,14 @@ class App extends Component {
       /* A JSX comment */
       <main className="container">
         <Navbar />
-        
+
 
         <Switch>
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
-
           <Route path="/not-found" component={NotFound} />
-          <Route path="/" exact component={Home} />
-          <Redirect from="/" to="/movies"/>
+          <Redirect from="/" exact to="/movies" />
           <Redirect to="/not-found" />
         </Switch>
 
