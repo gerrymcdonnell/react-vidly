7c6bc5a08564a047c491ffaa151141d4ee48b490
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-28 18:50:10 +0000

    updated navbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
-
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -27,15 +26,15 @@ const NavBar = () => {
           <NavLink className="nav-item nav-link" to="/customers">
             Customers
           </NavLink>
-
           <NavLink className="nav-item nav-link" to="/rentals">
             Rentals
           </NavLink>
-
           <NavLink className="nav-item nav-link" to="/login">
             Login
           </NavLink>
-
+          <NavLink className="nav-item nav-link" to="/register">
+            Register
+          </NavLink>
         </div>
       </div>
     </nav>
