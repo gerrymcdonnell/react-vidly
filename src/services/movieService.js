26e82e8f4e256caf106a5ab49864b9f0a65db716
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-28 09:15:05 +0000

    added updated movieform from tutorial

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,30 +2,30 @@
 import http from './httpService';
 import config from '../config.json';
 
+
+function movieUrl(id) {
+  return `${config.apiUrl}/${id}`;
+}
+
 //returns all movies
 export function getMovies() {
   return http.get(config.apiUrl+'/movies')
 }
 
 //1 movie
-/*export function getMovie(id) {
-  return movies.find(m => m._id === id);
-}*/
+export function getMovie(movieId) {
+  return http.get(movieUrl(movieId));
+}
 
-/*export function saveMovie(movie) {
-  let movieInDb = movies.find(m => m._id === movie._id) || {};
-  movieInDb.name = movie.name;
-  movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
-  movieInDb.numberInStock = movie.numberInStock;
-  movieInDb.dailyRentalRate = movie.dailyRentalRate;
-
-  if (!movieInDb._id) {
-    movieInDb._id = Date.now();
-    movies.push(movieInDb);
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(movieUrl(movie._id), body);
   }
 
-  return movieInDb;
-}*/
+  return http.post(config.apiUrl, movie);
+}
 
 export function deleteMovie(movieId) {
     return http.delete(config.apiUrl+'/movies/'+movieId);
