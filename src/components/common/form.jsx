300a9da5642f412850667d632bef9bd6713f8136
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-28 12:50:40 +0000

    movieform populating the form

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
 import Input from './input';
+import Select from './select';
 
 class Form extends Component {
     
@@ -81,6 +82,21 @@ class Form extends Component {
             error={errors[name]}
         />
     }
+
+    renderSelect(name, label, options) {
+        const { data, errors } = this.state;
+    
+        return (
+          <Select
+            name={name}
+            value={data[name]}
+            label={label}
+            options={options}
+            onChange={this.handleChange}
+            error={errors[name]}
+          />
+        );
+      }
 
 
 }
