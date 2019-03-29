3745a113f7c74545b2a7938bfe3ee528149d164d
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-29 16:25:44 +0000

    protected end points-enabling auth

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,41 +1,46 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
-
 //jwt decode npm i jwt-decode@2.2.0
 import jwtDecode from 'jwt-decode';
 
 const apiEndpoint = apiUrl + "/auth";
+const tokenKey = "token";
 
-const tokenKey="token";
+http.setJwt(getJWT());
 
-async function login(email,password){
-    const{data:jwt}=await http.post(apiEndpoint,{ email,password});
-    
+export async function login(email, password) {
+    const { data: jwt } = await http.post(apiEndpoint, { email, password });
     //store jwt in localstorage
-    localStorage.setItem(tokenKey,jwt);
+    localStorage.setItem(tokenKey, jwt);
 }
 
 
-function loginWithJwt(jwt){
-    localStorage.setItem(tokenKey,jwt);
+export function loginWithJwt(jwt) {
+    localStorage.setItem(tokenKey, jwt);
 }
 
-function logout(){
+export function logout() {
     localStorage.removeItem(tokenKey);
 }
 
-function getCurrentUser(){
+export function getCurrentUser() {
     try {
-        const jwt=localStorage.getItem(tokenKey);
-        return jwtDecode(jwt);        
-        } catch (ex) {   
-            return null;  
-        }
+        const jwt = localStorage.getItem(tokenKey);
+        return jwtDecode(jwt);
+    } catch (ex) {
+        return null;
+    }
 }
 
-export default{
+//get jwt from local storage
+export function getJWT() {
+    return localStorage.getItem(tokenKey);
+}
+
+export default {
     login,
     logout,
     getCurrentUser,
-    loginWithJwt
+    loginWithJwt,
+    getJWT
 }
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,21 +1,35 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
+//import auth from "./authService";
 
-import {toast} from 'react-toastify';
+//log service not implemented
+//import logger from "./logService";
+
+
+function setJwt(jwt){
+    //jwt token header to be sent which each request
+    axios.defaults.headers.common["x-auth-token"] =jwt;
+}
 
 //vid 146 interceptors
 axios.interceptors.response.use(null, err => {
     //expected error
-    const expectedError = err.response && err.response.status >= 400 && err.response.status < 500
+    const expectedError =
+        err.response && err.response.status >= 400 && err.response.status < 500
 
     if (!expectedError) {
         //logging the error somewhere
         console.log('Unexpected error', err);
-        
+
         //toast
         toast.error('unexpected error');
-        
+
         //information to user
         //toast('unexpected error');
+
+        //logservice not implemeted
+        //logger.log(error);
+
     }
     return Promise.reject(err);
 })
@@ -27,5 +41,6 @@ export default {
     get: axios.get,
     post: axios.post,
     put: axios.put,
-    delete: axios.delete
+    delete: axios.delete,
+    setJwt
 }
