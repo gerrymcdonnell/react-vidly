9461192966f1fd985f4f992de80fae3a9dcc2184
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-29 17:04:51 +0000

    protecting routes with protected route component

--- src/App.js
+++ src/App.js
@@ -17,10 +17,12 @@ import LoginForm from './components/loginForm';
 import RegisterForm from "./components/registerForm";
 
 import auth from './services/authService';
+import ProtectedRoute from './components/common/protectedRoute';
 
 import "./App.css";
 //import toastift css
 import 'react-toastify/dist/ReactToastify.css';
+
 
 
 class App extends Component {
@@ -44,7 +46,8 @@ class App extends Component {
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/logout" component={Logout} />
-            <Route path="/movies/:id" component={MovieForm} />
+
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
             
             {/* pass the user obect to the movies component. render attribute must be used */}
             <Route
